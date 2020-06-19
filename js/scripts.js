a1ae1aa4152eb51b6c614f5dd35b79079182c684
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-06-19 08:47:34 -0700

    add pizza business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,37 @@
+//Business logic
+
+//Pizza
+
+function Pizza (toppings, size) {
+  this.toppings = toppings;
+  this.size = size;
+}
+
+Pizza.prototype.cost = function () {
+  this.price = 0;
+  switch (this.size) {
+    case ("small"):
+      this.price += 10;
+      break;
+    case ("medium"):
+        this.price += 15;
+        break;
+    case ("large"):
+      this.price += 20;
+      break;
+  }
+  if (this.toppings.length > 3 && this.toppings.length < 7 && this.size !== "large") {
+    price += 6;
+  } else if (this.toppings.length >= 7 && this.size !== "large") {
+    price += 9;
+  } else {
+    price += 10;
+  }
+  return this.price;
+}
+
+
+
 $(document).ready(function(){
 
 });
